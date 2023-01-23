183a300de81d73167ec9f7b5540480310f2997d0
Rached ElBitar <rachedbitar10@gmail.com>
2023-01-23 20:27:37 +0200

    My name and major

---
+++ Me.java
@@ -0,0 +1,7 @@
+public class Me
+{
+             public static void main(String[] args){
+                  System.out.println("Rached ElBitar");
+                  System.out.println("Mechatronics engineering");
+          }
+}
